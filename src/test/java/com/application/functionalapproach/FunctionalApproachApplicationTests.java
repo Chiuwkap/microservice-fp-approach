58ea06fcfc20eb62a1a3caa4078c7d99557654c3
Chi Trinh <Chi.Trinh2@sigma.se>
2020-10-28 11:55:35 +0100

    initial project

---
+++ src/test/java/com/application/functionalapproach/FunctionalApproachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.functionalapproach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalApproachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
